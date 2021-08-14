bd674b30260059694c768c00309bb3b5c1cbe71e
Kevin Loftus <mrkevinjloftus@gmail.com>
2021-08-14 15:26:31 -0400

    Forwarding non-https requests

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(helmet.hsts());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+app.use((req, res, next) => {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+});
 app.use("/", express.static(path.join(__dirname, "ui")));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', "*");
