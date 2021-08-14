9bb0d46104f97ddec1453606e8e17f249e99aeec
Kevin Loftus <mrkevinjloftus@gmail.com>
2021-08-14 15:21:21 -0400

    Adding helmet hsts

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const app = express();
 
+app.use(helmet.hsts());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use("/", express.static(path.join(__dirname, "ui")));
